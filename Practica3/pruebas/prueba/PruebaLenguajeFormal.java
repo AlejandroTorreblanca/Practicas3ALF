52f068cdc06f14ca9b2618781c9fb7be85775630
Alejandro Torreblanca <alextorre2196@gmail.com>
2016-11-18 20:27:13 +0100

    Fin del tratamiento.
    El programa ya procesa correctamente los triangulos y los vectores. Hay
    que mejorar las expresiones regulares para que sean lo más precisas
    posibles y se adapten al formato requerido, habra que ir probando
    distintas entradas.
    De momento no deja mostrar los objetos en 3d, hay que arreglarlo.

--- Practica3/pruebas/prueba/PruebaLenguajeFormal.java
+++ Practica3/pruebas/prueba/PruebaLenguajeFormal.java
@@ -15,35 +15,14 @@ public class PruebaLenguajeFormal {
 		{
 			System.out.println("Escriba la Palabra:  ");
 		    String palabra=consola.nextLine(); //lee la línea con el nombre de fichero  desde consola			
-			
-		    
 
-		Pattern patLoop = Pattern.compile("^outer\\sloop");
-		Pattern patEndLoop = Pattern.compile("^endloop");
-		//Pattern patNormal = Pattern.compile("^facet\\snormal\\s  ");
-		//Pattern patNormal = Pattern.compile("^[-]*[\\d][.][\\d]+\\s[-]*[\\d]+[.][\\d]+\\s[-]*[\\d]+[.][\\d]+"); 	//funciona, caso: -5.32562 
-		
-		//Pattern patNormal = Pattern.compile("^[-]*[\\d][.][\\d]+[e|E][[-]|[+]][\\d]{1,2}");  						//funciona, caso:  3.235e-6
-		
-		
-		//Pattern patNormal = Pattern.compile("^facet\\snormal\\s[-]*[\\d][.][\\d]+[[e|E][[-]|[+]][\\d]{1,2}]*\\s[-]*[\\d]+[.][\\d]+[[e|E][[-]|[+]][\\d]{1,2}]*\\s[-]*[\\d]+[.][\\d]+[[e|E][[-]|[+]][\\d]{1,2}]*"); //Esta se supone que funciona
-		Pattern patNormal = Pattern.compile("^[-]*[\\d][.][\\d]+[[e|E][[-]|[+]][\\d]{1,2}]*");
-		
-		//Pattern patEndNormal = Pattern.compile("^endfacet");
-		Pattern patVertice = Pattern.compile("^vertex\\s[[\\d]+[.][\\d]+]{3}");
+		Pattern patNormal = Pattern.compile("^-?\\d\\.\\d+([eE][-+]\\d{1,2})?");
 		
 		Matcher matNormal = patNormal.matcher(palabra); //frase "facet normal ..."
 	    if (matNormal.matches()) 
-	    	System.out.println("Es es un patNormal ");
-	    Matcher matLoop = patLoop.matcher(palabra); //frase "outer loop"
-		if (matLoop.matches()) 
-			System.out.println("Es es un patLoop ");
-		Matcher matVertice = patVertice.matcher(palabra); //frase "vertex ..."
-		if (matVertice.matches()) 
-			System.out.println("Es es un patVertice ");
-		Matcher matEndLoop = patEndLoop.matcher(palabra); 
-		if (matEndLoop.matches()) 
-			System.out.println("Es es un patEndLoop ");
+	    	System.out.println("Correcto");
+	    else
+	    	System.out.println("NO");
 		}	
 		consola.close();
 	}
